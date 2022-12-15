a70f8805fd7fe4a7895ec70c96bd084ded7ffe28
seRRaloR <serralor@gmail.com>
2022-12-15 13:58:53 +0100

    Alta inicial proyecto

---
+++ spring-boot-data-jpa/src/test/java/com/jrla/springboot/app/SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrla.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
