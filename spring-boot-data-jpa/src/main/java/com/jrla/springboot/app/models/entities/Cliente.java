13964dca16cdb8ae206b3affbd2eef42093f1800
seRRaloR <serralor@gmail.com>
2022-12-15 14:46:17 +0100

    Faltaba una clase por salvar

--- spring-boot-data-jpa/src/main/java/com/jrla/springboot/app/models/entities/Cliente.java
+++ spring-boot-data-jpa/src/main/java/com/jrla/springboot/app/models/entities/Cliente.java
@@ -3,11 +3,14 @@ package com.jrla.springboot.app.models.entities;
 import java.io.Serializable;
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
 @Table(name = "clientes")
@@ -23,6 +26,53 @@ public class Cliente implements Serializable {
 	private String apellido;
 	private String email;
 	
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
 	private Date createAt;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	
 }
